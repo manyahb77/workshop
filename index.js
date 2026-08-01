8ca84ee2906256a3052c861314b5163bc22b1c98
manyahb <manyahb77@gmail.com>
2026-08-01 09:52:54 +0530

    qwrfrehytjyukluikui

---
+++ index.js
@@ -0,0 +1,6 @@
+const math=require('./math.js');
+console.log("Hello from NodeJS",math.Add(1,2));
+
+
+
+
---
+++ math.js
@@ -0,0 +1,12 @@
+function Add(a,b){
+    return a+b;
+}
+function Subtraction(a,b){
+    return a-b;
+   }
+module.exports={
+    Add,
+    Subtraction
+};
+
+    
